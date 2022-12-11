76fe35c62559b30f7a4168c98cafccfff8089ad0
dkmai <dkm.rade@gmail.com>
2022-12-11 17:14:06 -0500

    fixed left right movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -12,9 +12,15 @@ public class InputManager : MonoBehaviour
     private float jumpAmount = 10;
     private float maxSpeed = 5;
 
+    private SpriteRenderer renderer;
     // Start is called before the first frame update
     void Start()
     {
+        renderer = GetComponent<SpriteRenderer>();
+        if (renderer == null)
+        {
+            Debug.LogError("Player sprite is missing a renderer");
+        }
     }
 
     // Update is called once per frame
@@ -23,27 +29,28 @@ public class InputManager : MonoBehaviour
         //TODO Fix jumping, currently doesn't move properly with right or left, and does not jump high enough.
         Vector2 inputVector = Vector2.zero;
         isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        
         if (Input.GetKey("w") && isTouchingGround)
         {
-            rb.velocity =  new Vector2(rb.velocity.x, jumpAmount);
+            rb.velocity = new Vector2(rb.velocity.x, jumpAmount);
             
         }
-        /*if (Input.GetKey("w") && rb.velocity.y > 0f)
+        if (Input.GetKey("d") && isTouchingGround)
         {
-
-        }*/
-		if (Input.GetKey("a")) {
-			inputVector += Vector2.left;
-		}
-		if (Input.GetKey("d")) {
-			inputVector += Vector2.right;
-		}
-
+            inputVector += Vector2.right;
+            renderer.flipX = false;
+        }
+        else if (Input.GetKey("a") && isTouchingGround)
+        {
+            inputVector += Vector2.left;
+            renderer.flipX = true;
+        }
         if (inputVector != Vector2.zero)
         {
-            inputVector.Normalize();
+           //inputVector.Normalize();
             rb.velocity = inputVector * maxSpeed;
         }
+        
         
     }
 
